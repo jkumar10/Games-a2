62489faacf4561f077dd064bd5a52f07bb8b788f
Jainendra Kumar <jaikumar@iu.edu>
2018-10-15 00:29:45 -0400

    Updated rotate() and added minimax()

--- Betsy.py
+++ Betsy.py
@@ -1,30 +1,36 @@
 import sys
 from copy import deepcopy
 
+# METHOD TO ROTATE THE BOARD
 def rotate(state,n):
-    successor=[]
-    for col in range(0,n):
+    successor = []
+    dropPiece = []
+    for col in range(0, n):
         new_state = deepcopy(state)
         # SHIFTING EACH ELEMENT
-        removed_element = new_state[n][col]
-        for i in range(n, 0, -1):
-            if new_state[i - 1][col] != ".":
-                new_state[i][col] = new_state[i - 1][col]
-                new_state[i - 1][col] = "."
+        if new_state[(n + 3) - 1][col] != ".":
+            removed_element = new_state[(n + 3) - 1][col]
+            dropPiece.append(-(col + 1))
+            new_state[(n + 3) - 1][col] = "."
+            for i in range((n + 3) - 1, 0, -1):
+                if new_state[i - 1][col] != ".":
+                    new_state[i][col] = new_state[i - 1][col]
+                    new_state[i - 1][col] = "."
 
-        # DROPPING REMOVED PEBBLE
-        for j in range(n - 1, 0, -1):
-            flag=0
-            if new_state[j][col] == ".":
-                new_state[j][col] = removed_element
-                flag=1
-                break
-        if flag==0:
-            new_state[0][col]=removed_element
+            # DROPPING REMOVED PEBBLE
+            for j in range((n + 3) - 1, 0, -1):
+                flag = 0
+                if new_state[j][col] == ".":
+                    new_state[j][col] = removed_element
+                    flag = 1
+                    break
+            if flag == 0:
+                new_state[0][col] = removed_element
 
-        successor.append(new_state)
-    return successor
+            successor.append(new_state)
+    return dropPiece,successor
 
+# METHOD TO DROP THE BOARD
 def drop(state,n, turn):
     new_state=deepcopy(state)
     finalList=[]
@@ -36,18 +42,26 @@ def drop(state,n, turn):
                 addPiece.append(j)
                 finalList.append(new_state)
                 new_state=deepcopy(state)
-    return finalList
+    return addPiece,finalList
 
-def successor(board,n):
+# SUCCESSOR RETURNS ALL THE SUCCESSORS OF THE CURRENT STATE
+def successor(board,n,turn):
     succ=[]
-    l1=rotate(board,n)
-    l2=drop(board,n,'x')
+    move=[]
+    move1,l1=rotate(board,n)
+    move2,l2=drop(board,n,turn)
     for r in l1:
         succ.append(r)
     for d in l2:
         succ.append(d)
-    return succ
+    for m1 in move1:
+        move.append(m1)
+    for m2 in move2:
+        move.append(m2)
 
+    return succ,move
+
+# METHOD TO CHECK IF THE CURRENT STATE IS A GOAL STATE
 def isGoal(board,n):
     # CHECKING ALL ROWS
     i=0
@@ -79,6 +93,32 @@ def isGoal(board,n):
     if all(element == 'x' for element in rightdiag) == True or all(element == 'o' for element in rightdiag) == True:
         return True
 
+# MINIMAX ALGORITHM Include time as well
+def minimax(state,n,depth,alpha,beta, player):
+    if depth ==0 or isGoal(state,n):
+        # return heuristic value
+        return 50
+    if player=="x":
+        maxEval=-100000000
+        for child in successor(state,n,"x"):
+            eval=minimax(child,n,depth-1,alpha,beta,"o")
+            maxEval=max(maxEval,eval)
+            alpha=max(alpha,eval)
+
+            if beta<=alpha:
+                break
+        return maxEval
+    else:
+        minEval=100000000
+        for child in successor(state,n,"o"):
+            eval = minimax(child,n, depth - 1, alpha, beta, "x")
+            minEval=min(minEval,eval)
+            beta=min(beta,eval)
+            #print(minEval)
+            if beta<=alpha:
+                break
+        return minEval
+
 
 
 # TAKING INPUTS AS COMMANDLINE
@@ -98,5 +138,8 @@ for j in range(n+3):
 
 
 # WILL RETURN TRUE IF GOAL FOUND OTHERWISE FALSE OR NONE
-print successor(initial_board,n)
+a,b=successor(initial_board,n,'x')
+print a
+print b
 #print isGoal(initial_board,n)
+#print minimax(initial_board,n,3,-100000000,100000000,"x")
